28baf7d24d8a068129a6966b186010e71f6fbec5
alexpr465 <alejandro.pr.1980@gmail.com>
2020-07-01 13:43:23 -0500

    Proyecto Maven inicial

---
+++ src/main/java/com/apr/main/app_ejemplo/App.java
@@ -0,0 +1,19 @@
+package com.apr.main.app_ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        EjecutarFuncion();
+    }
+    
+    public static void EjecutarFuncion() {
+    	
+    	System.out.println("Hola dos");
+    }
+}

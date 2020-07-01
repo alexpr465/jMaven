ca496d7b9e96c7942860c25f657fcd0141494375
alexpr465 <alejandro.pr.1980@gmail.com>
2020-07-01 15:56:30 -0500

    Rama alterna 1. Se agrega clase

---
+++ src/main/java/com/apr/main/app_ejemplo/cDatosPersonales.java
@@ -0,0 +1,11 @@
+package com.apr.main.app_ejemplo;
+
+public class cDatosPersonales {
+	public String sDireccion;
+	public String sCalle;
+	public String sNumero;
+	public String sCodigoPostal;
+	public String sDelegacion;
+	public String sEstado;
+
+}
